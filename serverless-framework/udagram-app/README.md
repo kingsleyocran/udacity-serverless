36aba82e9a4c5928ef9bb8a52c268b32d7a3356e
kingsleyocran <ocrankingsley@gmail.com>
2022-07-20 15:57:36 +0000

    add files for serverless-framework

---
+++ serverless-framework/udagram-app/README.md
@@ -0,0 +1,1 @@
+#Serverless AWS NodeJS TS Template
---
+++ serverless-framework/udagram-app/set_env.sh
@@ -0,0 +1,3 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export GROUP_TABLE=Groups-dev
